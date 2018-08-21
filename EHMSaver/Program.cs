b0be270d533788e6227105cdb10eabbe0408f442
DeadPlatypus <pyromaniak5@gmail.com>
2018-08-21 15:12:08 +0200

    Basic code

--- EHMSaver/Program.cs
+++ EHMSaver/Program.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Discord.WebSocket;
 
 namespace EHMSaver
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
+                => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
         {
+            var client = new DiscordSocketClient();
+            //comment
+            client.Log += Log;
+
+            string token = System.IO.File.ReadAllText(@"X:\Users\Alain\source\repos\token.txt");
+            await client.LoginAsync(Discord.TokenType.Bot, token);
+            await client.StartAsync();
+
+            await Task.Delay(-1);
         }
     }
 }
